c0aa264c200a2f267ac63696c7b671a73ac5e191
Jamie Jasmine Koh <132534869+JamieKoh@users.noreply.github.com>
2023-05-12 00:01:03 +1000

    created the main page
    created the main page but without dynamic functions that pull data from the database along with all the files required to do so

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/sass/main.scss";
 import Navbar from "./components/Navbar";
 import Upload from "./components/Upload"; 
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <BrowserRouter>
+    < MainPage />
     <Navbar />
+    
       <Routes>
         <Route path="/upload" element={<Upload />} />
+        <Route path="/mainpage" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
