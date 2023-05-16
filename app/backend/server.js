72ac633f8ce8306e556a82c65a309f9e8c11a054
Matthew Smith <85216187+mattsmith135@users.noreply.github.com>
2023-05-16 13:00:08 +1000

    MySQL migration: package removal, routes removal, MySQL initial setup
    Uninstall packages and files related to MongoDB backend. Installed MySQL and wrote basic setup code.
    
    Packages uninstalled:
    - Mongoose
    
    Files removed:
    - .env.example
    - all MongoDB models
    - all routes
    
    Packages installed:
    - mysql

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,28 +1,31 @@
 const express = require('express'); 
 const cors = require('cors');
-const mongoose = require('mongoose'); 
+const mysql = require('mysql'); 
 
 require('dotenv').config(); 
 
 const app = express(); 
 const port = process.env.PORT || 5000; 
 
-const uri = process.env.ATLAS_URI; 
-mongoose.connect(uri, { useNewURLParser: true }
-); 
-const connection = mongoose.connection; 
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully")
-})
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'your_user', 
+    password: 'your_password', 
+    database: 'ouch_button',
+}); 
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connnecting to MySQL database', err); 
+    } else {
+        console.log('Connected to MySQL database'); 
+    }
+}); 
+
+// Define API Routes
 
 app.use(cors()); 
 app.use(express.json());
-
-const clientsRouter = require('./routes/clients'); 
-const therapistsRouter = require('./routes/therapists');
-
-app.use('/clients', clientsRouter); 
-app.use('/therapists', therapistsRouter); 
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
