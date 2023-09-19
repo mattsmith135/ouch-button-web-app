991fb7ce2ac493452b4e8a54cc18a389ec59d347
haze1511 <rubenahayes@gmail.com>
2023-09-19 17:26:43 +1000

    Changed from ejs to js

---
+++ app/client/src/components/register.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function register(){
+    return(
+        <div>
+            <h1>Register</h1>
+<form action="/register" method="POST">
+    <div>
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" required/>
+    </div>
+    <div>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" required/>
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required/>
+    </div>
+    <button type="submit">Register</button>
+</form>
+
+
+<a href="/login">Login</a>
+
+</div>
+)
+}
