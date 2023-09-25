5a4308dd2475f908f7acc2bd0868408c64bf5cca
Matthew Smith <matthew.smith2k@gmail.com>
2023-09-25 21:45:18 +1000

    Finished connection of upload page to database
    Commits code that creates a connection between the upload page.
    Users can now upload an ouch-button-generated file have its contents extracted and uploaded to the database
    
    KEY CHANGES:
    - removed 'Location' field in schema. Added new 'Latitude' and 'Longitude' fields. Also changed dummy data accordingly
    - moved file upload server logic to the ouchbuttondata route file
    - integrated SearchableDropdown selected value into file upload axios post request

--- app/server/server.js
+++ app/server/server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); 
 const cors = require('cors');
+const db = require('./models');
 const ouchbuttondataRouter = require('./routes/ouchbuttondata');
 const clientdataRouter = require('./routes/clientdata');
 const therapistdataRouter = require('./routes/therapistdata');
-const port = process.env.EXPRESS_PORT || 5000;
-const db = require('./models');
-const multer = require('multer');
-const path = require('path'); 
+const port = process.env.EXPRESS_PORT || 5000; 
 
 require('dotenv').config(); 
 
@@ -26,32 +24,6 @@ db.sequelize.sync().then(() => {
         console.log(`Server is running on port: ${port}`);
     }); 
 });
-
-// Define a storage engine for Multer to store uploaded files
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-// Route for handling file uploads
-app.post('/api/upload', upload.single('file'), async (req, res) => {
-    try {
-        if (!req.file) {
-            return res.status(400).json({ message: 'No file uploaded' });
-        }
-
-        // Process the uploaded file 
-        const content = req.file.buffer.toString();
-        
-        console.log(content); 
-
-        // Insert into Sequelize model
-        // TBC
-
-        // res.status(200).json({ message: 'File uploaded and data inserted', result }); 
-    } catch (error) {
-        console.error('Error uploading file:', error)
-        res.status(500).json({ message: 'Error uploading file' }); 
-    }
-})
 
 // Login server code
 
@@ -91,7 +63,7 @@ app.get('/login', checkNotAuthenticated, (req, res) => {
   res.render('login.ejs')
 })
 
-// If login was successfu, get index
+// If login was successful, get index
 // If not, go back to login
 app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
   successRedirect: '/',
