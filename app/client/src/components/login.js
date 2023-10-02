ac85056b2a5029ac4a16f8b2b377ea4c8de61a5d
haze1511 <rubenahayes@gmail.com>
2023-10-02 17:59:41 +1100

    Got styling to work again after the merge

--- app/client/src/components/login.js
+++ app/client/src/components/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import logo from "../assets/logo.png"; 
 
 function Login() {
     const [loginEmail, setLoginEmail] = useState("");
@@ -27,14 +28,15 @@ function Login() {
                 </div>
                 <div className="login-content">
                     <form id="login-form" action="/login" method="POST">
-                        <div>
+                        <div className="EmailLogin">
                             <label>Email</label>
                             <input type="email" id="email" name="email" required onChange={e => setLoginEmail(e.target.value)} />
                         </div>
-                        <div>
+                        <div className="PasswordLogin">
                             <label>Password</label>
                             <input type="password" id="password" name="password" required onChange={e => setLoginPassword(e.target.value)} />
                         </div>
+                        <br></br>
                         <button type="submit" onClick={login}>Login</button>
                     </form>
                 </div>
